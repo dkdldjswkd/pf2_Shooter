2d5822c37e1cd4abba68b43b0aa9f73976f70700
dkdldjswkd <dkdldjswkd@naver.com>
2020-09-11 03:24:54 +0900

    gun 스크립트 구현중

---
+++ Shooter/Assets/Script/GunController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public Transform weaponHold;
+    Gun equippedGun;
+
+    public void EquipGun(Gun ghunToEquip)
+    {
+        if (equippedGun != null)
+            Destroy(equippedGun.gameObject);
+        equippedGun = Instantiate(ghunToEquip, weaponHold.position, weaponHold.rotation);
+    }
+
+
+}
