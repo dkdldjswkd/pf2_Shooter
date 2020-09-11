248ad2a6eabca2114ac9a4ee73ca00c73e9d1b49
dkdldjswkd <dkdldjswkd@naver.com>
2020-09-12 02:17:30 +0900

    shoot 구현

--- Shooter/Assets/Script/GunController.cs
+++ Shooter/Assets/Script/GunController.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class GunController : MonoBehaviour
 {
     public Transform weaponHold;
+    public Gun startingGun;
     Gun equippedGun;
+    
+    private void Start()
+    {
+        if(startingGun != null)
+        {
+            EquipGun(startingGun);
+        }
+    }
 
     public void EquipGun(Gun ghunToEquip)
     {
         if (equippedGun != null)
             Destroy(equippedGun.gameObject);
-        equippedGun = Instantiate(ghunToEquip, weaponHold.position, weaponHold.rotation);
+        equippedGun = Instantiate(ghunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold;
     }
 
+
+    public void Shoot()
+    {
+        if(equippedGun != null)
+        {
+            equippedGun.Shoot();
+        }
+    }
 
 }
--- Shooter/Assets/Script/Player.cs
+++ Shooter/Assets/Script/Player.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof(PlayerController))]
+[RequireComponent (typeof(GunController))]
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5;
     PlayerController controller;
     Camera viewCamera;
+    GunController gunController;
 
     // Start is called before the first frame update
     void Start()
     {
+        gunController = GetComponent<GunController>();
         controller = GetComponent<PlayerController>();
         viewCamera = Camera.main;
     }
@@ -19,10 +22,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //이동을 입력받는곳
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
+        //바라보는 방향을 입력받는곳
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlain = new Plane(Vector3.up, Vector3.zero); // 노멀벡터, 원점으로부터 거리
         float rayDistance;
@@ -34,5 +39,10 @@ public class Player : MonoBehaviour
             controller.LookAt(point);
         }
 
+        //무기조작입력
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
+        }
     }
 }
