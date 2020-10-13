e836ed1eabba01dc539e2d3ef839fef672307a50
dkdldjswkd <dkdldjswkd@naver.com>
2020-10-13 12:41:58 +0900

    타일 맵 생성중

---
+++ Shooter/Assets/Editor/MapEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof(MapGenerator)) ]
+public class MapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+          MapGenerator map = target as MapGenerator;
+
+
+
+        map.GenerateMap();
+    }
+}
